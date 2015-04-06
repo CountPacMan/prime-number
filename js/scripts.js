7a796ba60600872878641eae785fd35d8cfbff47
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-06 15:40:22 -0700

    fix primes logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
 function getPrimes(number) {
   var primes = [2];
+  // initialize primes array to include all number from 2 - number
   for (var i = 3; i <= number; i++) {
     primes.push(i);
+  }
+
+  // remove all multiples of known primes by iterating through starting at 2
+  var primes_length = primes.length;
+  console.log("pl = " + primes_length);
+  for (var i = 2; i <= number && i < primes.length; i++) {
+    console.log("i = " + i + ", j = " + j);
+    var j = 1;
+    while (j < primes.length) {
+      if (primes[j] % i === 0) {
+        console.log("i = " + i + ", j = " + j);
+        console.log("will be del " + primes[j]);
+        primes.splice(j, 1);
+        console.log("current primes " + primes);
+        console.log("j is " + j);
+      } else {
+        j++;
+      }
+    }
+
+    // for (var j = 1; j < primes_length; j++) {
+    //   if (primes[j] % i === 0) {
+    //     console.log("i = " + i + ", j = " + j);
+    //     console.log("will be del " + primes[j]);
+    //     primes.splice(j, 1);
+    //     console.log("current primes " + primes);
+    //     j = j - 1;
+    //     console.log("j is " + j);
+    //   }
+    // }
   }
   return primes;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,22 @@
 describe('getPrimes', function() {
-  it("returns prime numbers of 2 to equal 2", function() {
-    expect(getPrimes(2)).to.deep.equal([2]);
-  });
+  // it("returns prime numbers of 2 to equal 2", function() {
+  //   expect(getPrimes(2)).to.deep.equal([2]);
+  // });
+  //
+  // it("returns prime numbers of 3 to equal [2, 3]", function() {
+  //   expect(getPrimes(3)).to.deep.equal([2, 3]);
+  // });
+  //
+  // it("returns prime numbers of 4 to equal [2, 3]", function() {
+  //   expect(getPrimes(4)).to.deep.equal([2, 3]);
+  // });
+  //
+  // it("returns prime numbers of 5 to equal [2, 3, 5]", function() {
+  //   expect(getPrimes(5)).to.deep.equal([2, 3, 5]);
+  // });
 
-  it("returns prime numbers of 3 to equal [2, 3]", function() {
-    expect(getPrimes(3)).to.deep.equal([2, 3]);
+  it("returns prime numbers of 6 to equal [2, 3, 5]", function() {
+    expect(getPrimes(6)).to.deep.equal([2, 3, 5]);
   });
 
 });
